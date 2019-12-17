3252bef40027017083cbeab37ea1cf0a5d13b5c9
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2019-12-17 08:50:15 +0300

    add AppoinmentEntity

--- src/main/java/com/github/dao/AppointmentDAO.java
+++ src/main/java/com/github/dao/AppointmentDAO.java
@@ -1,10 +1,12 @@
 package com.github.dao;
 
+import com.github.model.AppointmentEntity;
+
 public interface AppointmentDAO {
 
-    void addAppointment();
+    void addAppointment(AppointmentEntity appointmentEntity);
     void removeAppointment(int id);
-    void updateAppointment(int id);
+    void updateAppointment(AppointmentEntity appointmentEntity);
     boolean checkAppointment(int id);
 
 
--- src/main/java/com/github/dao/AppointmentDAOImpl.java
+++ src/main/java/com/github/dao/AppointmentDAOImpl.java
@@ -1,8 +1,12 @@
 package com.github.dao;
 
+import com.github.model.AppointmentEntity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AppointmentDAOImpl implements AppointmentDAO {
 
     private final SessionFactory sessionFactory;
@@ -14,22 +18,34 @@ public class AppointmentDAOImpl implements AppointmentDAO {
     }
 
     @Override
-    public void addAppointment() {
-
+    public void addAppointment(AppointmentEntity appointmentEntity) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(appointmentEntity);
     }
 
     @Override
     public void removeAppointment(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        AppointmentEntity appointmentEntity = session.get(AppointmentEntity.class, id);
 
+        if (appointmentEntity != null) {
+            session.delete(appointmentEntity);
+        }
     }
 
     @Override
-    public void updateAppointment(int id) {
-
+    public void updateAppointment(AppointmentEntity appointmentEntity) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(appointmentEntity);
     }
 
     @Override
     public boolean checkAppointment(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        AppointmentEntity appointmentEntity = session.get(AppointmentEntity.class, id);
+        if (appointmentEntity.getId()==id){
+            return true;
+        }
         return false;
     }
 }
